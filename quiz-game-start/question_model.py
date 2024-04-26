d4bfd7a6169545543b624517578b7f636dd95aa2
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2024-04-26 12:48:36 -0600

    Trivia Game

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,7 @@
+class Question:
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+q1 = Question("What is the average velocity of an African Swallow?", "29 MPH")
